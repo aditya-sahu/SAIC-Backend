0e38fd815af72cd45cec51808aac969d3de33b1e
Aditya Sahu <adityasahu1511@gmail.com>
2020-04-13 13:26:48 +0530

    changes in API

--- app/controllers/student.create.js
+++ app/controllers/student.create.js
@@ -42,33 +42,35 @@ exports.create = (req, res) => {
 // Update exsiting student's activity
 exports.update = (req, res) => {
     console.log(req.body);
-    // Validate request
-    if(!req.body.name) {
-        return res.status(400).send({
-            status: 400,
-            message: "name cannot be empty"
-        });
-    }
-
-    // Create a new Student
-    const student = new Student({
-        name: req.body.name,
-        headActivity: req.body.headActivity || "NA",
-        presenceActivity: req.body.presenceActivity || "NA",
-        emotionActivity: req.body.emotionActivity || "NA"
-    });
-
-    // Save Student into the database
-    student.save()
-    .then(data => {
-        res.send({
-            status:200,
-            data:data,
-            message:'Student created successfully'});
-    }).catch(err => {
-        res.status(500).send({
-            status:500,
-            message: err.message || "Some error occurred while creating the Student."
-        });
-    });
+    const _name = req.body.name;
+    Student.findOne({"name":_name})
+    .then(student => {
+        if(!student) {
+            return res.status(400).send({
+                status:400,
+                message: "Student not found."
+            });
+        }
+        // Update activityData of the user
+        const _angle = req.body.headActivity.angle;
+        const _dateTimeNow = Date.now();
+        Student.updateOne(
+            {"name": _name},
+            {$push: {"headActivity": {angle:_angle,ts:_dateTimeNow}}}
+        )
+        .then(data => {
+            if(!data ) {
+                return res.status(404).send({
+                    status:404,
+                    message: "No data found. "
+                });            
+            }
+            res.send(data);
+        }).catch(err => {
+            return res.status(500).send({
+                status:500,
+                message: err.message || "Error updating data with given parameters"
+            });
+        })
+    })
 };
